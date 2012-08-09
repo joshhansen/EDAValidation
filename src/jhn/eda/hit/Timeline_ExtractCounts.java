c0a324498b4d426f8b6a14e0120969827ed334af
Josh Hansen <hansen.joshuaa@gmail.com>
2012-08-09 12:31:49 -0600

    Code for generating a timeline experiment

---
+++ src/jhn/eda/hit/Timeline_ExtractCounts.java
@@ -0,0 +1,85 @@
+package jhn.eda.hit;
+
+import java.io.File;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.FSDirectory;
+
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.InstanceList;
+
+import jhn.eda.CountsExtractor;
+import jhn.eda.Paths;
+import jhn.eda.topiccounts.LuceneTopicCounts;
+import jhn.eda.topiccounts.TopicCounts;
+import jhn.eda.topictypecounts.LuceneTopicTypeCounts;
+import jhn.eda.topictypecounts.TopicTypeCounts;
+import jhn.eda.typetopiccounts.LuceneTypeTopicCounts;
+import jhn.eda.typetopiccounts.TypeTopicCounts;
+import jhn.util.Util;
+
+public class Timeline_ExtractCounts implements AutoCloseable {
+	private final String topicWordIdxName;
+	private IndexReader topicWordIdx;
+	private TopicCounts srcTopicCounts;
+
+	public Timeline_ExtractCounts(String topicWordIdxName) throws Exception {
+		this.topicWordIdxName = topicWordIdxName;
+		String topicWordIdxLuceneDir = Paths.topicWordIndexDir(topicWordIdxName);
+		topicWordIdx = IndexReader.open(FSDirectory.open(new File(topicWordIdxLuceneDir)));
+		srcTopicCounts = new LuceneTopicCounts(topicWordIdx);
+	}
+	
+	public void extract(String datasetName, String datasetFilename) throws Exception {
+		// Config
+		int minCount = 2;
+		
+		System.out.println("Extracting " + datasetName);
+		
+		String topicMappingFilename =              Paths.topicMappingFilename(topicWordIdxName, datasetName, minCount);
+		String propsFilename =                 Paths.propsFilename(topicWordIdxName, datasetName, minCount);
+		String topicCountsFilename =           Paths.topicCountsFilename(topicWordIdxName, datasetName, minCount);
+		String restrictedTopicCountsFilename = Paths.restrictedTopicCountsFilename(topicWordIdxName, datasetName, minCount);
+		String filteredTopicCountsFilename =   Paths.filteredTopicCountsFilename(topicWordIdxName, datasetName, minCount);
+		String typeTopicCountsFilename =       Paths.typeTopicCountsFilename(topicWordIdxName, datasetName, minCount);
+		
+		// Load
+		InstanceList targetData = InstanceList.load(new File(datasetFilename));
+		Alphabet typeAlphabet = (Alphabet) targetData.getAlphabet().clone();
+		final int typeCount = typeAlphabet.size();
+
+		targetData = null;
+		
+		TypeTopicCounts srcTypeTopicCounts = new LuceneTypeTopicCounts(topicWordIdx, typeAlphabet);
+		TopicTypeCounts srcTopicTypeCounts = new LuceneTopicTypeCounts(topicWordIdx, typeAlphabet);
+		
+		// Run
+		try(CountsExtractor ce = new CountsExtractor(srcTopicCounts, srcTypeTopicCounts, srcTopicTypeCounts,
+				typeCount, minCount, topicMappingFilename, propsFilename, topicCountsFilename, restrictedTopicCountsFilename,
+				filteredTopicCountsFilename, typeTopicCountsFilename)) {
+			
+			ce.extract();
+		}
+	}
+
+	@Override
+	public void close() throws Exception {
+		Util.closeIfPossible(srcTopicCounts);
+		topicWordIdx.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		final int window = 40;
+		
+		try(Timeline_ExtractCounts tec = new Timeline_ExtractCounts("wp_lucene4")) {
+			final String timelineDir = jhn.Paths.outputDir("LDA") + "/datasets/sotu_timeline";
+			for(int startYear = 1790; startYear < 2000; startYear += 20) {
+				int endYear = startYear + window;
+				
+				String datasetName = "sotu_" + startYear + "-" + endYear;
+				String datasetFilename = timelineDir + "/" + startYear + "-" + endYear + ".mallet";
+				tec.extract(datasetName, datasetFilename);
+			}
+		}
+	}
+}
---
+++ src/jhn/eda/hit/Timeline_RunEDA.java
@@ -0,0 +1,68 @@
+package jhn.eda.hit;
+
+import java.io.File;
+
+import cc.mallet.types.InstanceList;
+
+import jhn.eda.EDA;
+import jhn.eda.Options;
+import jhn.eda.Paths;
+import jhn.eda.topiccounts.TopicCounts;
+import jhn.eda.topicdistance.LuceneTopicDistanceCalculator;
+import jhn.eda.topicdistance.TopicDistanceCalculator;
+import jhn.eda.typetopiccounts.TypeTopicCounts;
+import jhn.util.Config;
+import jhn.util.ConstFactory;
+import jhn.util.Factory;
+import jhn.util.Util;
+
+public class Timeline_RunEDA {
+	public static void run() throws Exception {
+		final int window = 40;
+		final int iterations = 500;
+		final int minCount = 2;
+		final String topicWordIdxName = "wp_lucene4";
+		
+		for(int startYear = 1790; startYear < 2000; startYear += 20) {
+			int endYear = startYear + window;
+			
+			String datasetName = "sotu_" + startYear + "-" + endYear;
+			
+			System.out.print("Loading type-topic counts...");
+			final String ttCountsFilename = Paths.typeTopicCountsFilename(topicWordIdxName, datasetName, minCount);
+			TypeTopicCounts ttcs = (TypeTopicCounts) Util.deserialize(ttCountsFilename);
+			System.out.println("done.");
+
+			System.out.print("Loading topic counts...");
+			final String topicCountsFilename = Paths.filteredTopicCountsFilename(topicWordIdxName, datasetName, minCount);
+			TopicCounts tcs = (TopicCounts) Util.deserialize(topicCountsFilename);
+			Factory<TopicCounts> tcFact = new ConstFactory<>(tcs);
+			System.out.println("done.");
+
+			Config props = (Config) Util.deserialize(Paths.propsFilename(topicWordIdxName, datasetName, minCount));
+			
+			TopicDistanceCalculator tdc = new LuceneTopicDistanceCalculator(null, null);
+			EDA eda = new EDA (tcFact, ttcs, tdc, props.getInt(Options.NUM_TOPICS), Paths.nextRun());
+			eda.conf.putInt(Options.PRINT_INTERVAL, 1);
+			eda.conf.putBool(Options.PRINT_FAST_STATE, true);
+			eda.conf.putDouble(Options.ALPHA_SUM, 10000);
+			eda.conf.putDouble(Options.BETA, 0.01);
+			eda.conf.putInt(Options.ITERATIONS, iterations);
+			eda.conf.putInt(Options.MIN_THREADS, Runtime.getRuntime().availableProcessors());
+			eda.conf.putInt(Options.MAX_THREADS, Runtime.getRuntime().availableProcessors()*3);
+			
+			System.out.print("Loading target corpus...");
+			InstanceList targetData = InstanceList.load(new File(Paths.datasetFilename(datasetName)));
+			System.out.println("done.");
+			System.out.print("Processing target corpus...");
+			eda.setTrainingData(targetData);
+			System.out.println("done.");
+			
+			eda.sample();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		run();
+	}
+}
