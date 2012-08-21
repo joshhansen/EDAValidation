34531c399ce2c1f7d3d740400d6fdb71e66f7c1a
Josh Hansen <hansen.joshuaa@gmail.com>
2012-08-21 00:19:06 -0600

    Merge stuff

---
+++ src/jhn/calibration/topiccount/CalibrationMerge.java
@@ -0,0 +1,149 @@
+package jhn.calibration.topiccount;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+
+import jhn.util.DefaultMap;
+import jhn.util.Factory;
+import jhn.util.RandUtil;
+import jhn.validation.BareLabelSource;
+import jhn.validation.LabelSource;
+import jhn.validation.Paths;
+import jhn.validation.StandardTopicLabelSource;
+import jhn.validation.TopicLabelSource;
+
+public class CalibrationMerge<T> {
+	private final File srcDir;
+	private final String destFilename;
+	private final int comparisonsPerPair;
+	private final LabelSourceFactory labelSrcFact;
+	public CalibrationMerge(String srcDir, String destFilename, int comparisonsPerPair, LabelSourceFactory labelSrcFact) {
+		this.srcDir = new File(srcDir);
+		this.destFilename = destFilename;
+		this.comparisonsPerPair = comparisonsPerPair;
+		this.labelSrcFact = labelSrcFact;
+	}
+	
+	/** topicCount -> run -> label source */
+	private class Labels {
+		private Int2ObjectMap<Int2ObjectMap<BareLabelSource>> labels = new Int2ObjectOpenHashMap<>();
+		
+		public void setLabelSource(int topicCount, int run, BareLabelSource source) {
+			getLabels(topicCount).put(run, source);
+		}
+		
+		private Int2ObjectMap<BareLabelSource> getLabels(int topicCount) {
+			Int2ObjectMap<BareLabelSource> map = labels.get(topicCount);
+			if(map==null) {
+				map = new Int2ObjectOpenHashMap<>();
+				labels.put(topicCount, map);
+			}
+			return map;
+		}
+		
+		private BareLabelSource getLabels(int topicCount, int run) {
+			return getLabels(topicCount).get(run);
+		}
+	}
+
+	public interface LabelSourceFactory {
+		BareLabelSource create(File file);
+	}
+	
+	
+	
+	public void run() throws FileNotFoundException, IOException {
+		
+		// Load label lines from disk
+		Labels labels = new Labels();
+		for(File file : srcDir.listFiles()) {
+			Matcher m = jhn.validation.Paths.NAME_RGX.matcher(file.getName().split("[.]")[0]);
+			m.matches();
+			int topicCount = Integer.parseInt(m.group(1));
+			int run = Integer.parseInt(m.group(2));
+			
+			labels.setLabelSource(topicCount, run, labelSrcFact.create(file));
+		}
+		
+		List<String> output = new ArrayList<>();
+		for(Int2ObjectMap.Entry<Int2ObjectMap<BareLabelSource>> entry1 : labels.labels.int2ObjectEntrySet()) {
+			
+			final int topicCount1 = entry1.getIntKey();
+			final int[] runs1 = entry1.getValue().keySet().toIntArray();
+			
+			for(Int2ObjectMap.Entry<Int2ObjectMap<BareLabelSource>> entry2 : labels.labels.int2ObjectEntrySet()) {
+				final int topicCount2 = entry2.getIntKey();
+				final int[] runs2 = entry2.getValue().keySet().toIntArray();
+				
+				if(topicCount1 != topicCount2) {
+					for(int cmpNum = 0; cmpNum < comparisonsPerPair; cmpNum++) {
+						int run1 = RandUtil.randItem(runs1);
+						int run2 = RandUtil.randItem(runs2);
+						
+						String labelLine1 = labels.getLabels(topicCount1, run1).labels(1)[0];
+						String labelLine2 = labels.getLabels(topicCount2, run2).labels(1)[0];
+						
+						StringBuilder outputLine = new StringBuilder();
+						outputLine.append(topicCount1).append(',').append(run1).append(',');
+						outputLine.append(topicCount2).append(',').append(run2).append(',');
+						outputLine.append(labelLine1).append(',').append(labelLine2);
+						output.add(outputLine.toString());
+					}
+				}
+			}
+		}
+		
+		// Output comparisons
+		try(PrintWriter w = new PrintWriter(new FileWriter(destFilename))) {
+			final String[] sides = new String[]{"1","2"};
+			for(String side : sides) {
+				w.append("topicCount").append(side).append(',');
+				w.append("run").append(side).append(',');
+			}
+			
+			for(int i = 0; i < sides.length; i++) {
+				String side = sides[i];
+				w.append("topic").append(side).append(',');
+				
+				for(int wordNum = 0; wordNum < 20; wordNum++) {
+					w.append("model").append(side).append("word").append(String.valueOf(wordNum)).append(',');
+				}
+				w.append("model").append(side).append("label");
+				if(i < sides.length - 1) w.append(',');
+			}
+			w.append('\n');
+			
+			Collections.shuffle(output);
+			Collections.shuffle(output);
+			Collections.shuffle(output);
+			Collections.shuffle(output);
+			Collections.shuffle(output);
+			
+			for(String outputLine : output) {
+				w.append(outputLine).append('\n');
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		final int comparisonsPerPair = 10;
+		String datasetName = "reuters21578";
+		CalibrationMerge tccm = new CalibrationMerge(
+				Paths.topicCountCalibrationLauTopicLabelsDir(datasetName),
+				Paths.topicCountCalibrationMergedTopicLabelsFilename(datasetName),
+				comparisonsPerPair);
+		tccm.run();
+	}
+}
