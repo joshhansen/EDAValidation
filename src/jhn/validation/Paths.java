a0b391941322ef59806de2a0d76e7851a8ed7a9b
Josh Hansen <hansen.joshuaa@gmail.com>
2012-08-23 16:13:17 -0600

    Use shared file extensions in jhn.Paths

--- src/jhn/validation/Paths.java
+++ src/jhn/validation/Paths.java
@@ -44,9 +44,8 @@ public class Paths {
 		return topicCountCalibrationDir(dataset) + "/state";
 	}
 	
-	public static final String STATE_EXT = ".state";
 	public static String topicCountCalibrationStateFilename(String dataset, int numTopics, int run) {
-		return topicCountCalibrationStateDir(dataset) + "/" + name(numTopics, run) + STATE_EXT;
+		return topicCountCalibrationStateDir(dataset) + "/" + name(numTopics, run) + jhn.Paths.STATE_EXT;
 	}
 	
 	public static String topicCountCalibrationLogFilename(String dataset) {
@@ -57,27 +56,24 @@ public class Paths {
 		return topicCountCalibrationDir(dataset) + "/keys";
 	}
 	
-	public static String KEYS_EXT = ".keys";
 	public static String topicCountCalibrationKeysFilename(String dataset, int numTopics, int run) {
-		return topicCountCalibrationKeysDir(dataset) + "/" + name(numTopics, run) + KEYS_EXT;
+		return topicCountCalibrationKeysDir(dataset) + "/" + name(numTopics, run) + jhn.Paths.KEYS_EXT;
 	}
 	
-	public static final String TOPIC_LABELS_EXT = ".topic_labels";
 	public static String topicCountCalibrationLauTopicLabelsDir(String dataset) {
 		return topicCountCalibrationDir(dataset) + "/lau_topic_labels";
 	}
 	
 	public static String topicCountCalibrationLauTopicLabelsFilename(String dataset, int numTopics, int run) {
-		return topicCountCalibrationLauTopicLabelsDir(dataset) + "/" + name(numTopics, run) + TOPIC_LABELS_EXT;
+		return topicCountCalibrationLauTopicLabelsDir(dataset) + "/" + name(numTopics, run) + jhn.Paths.TOPIC_LABELS_EXT;
 	}
 	
-	public static final String DOC_LABELS_EXT = ".doc_labels";
 	public static String topicCountCalibrationLauDocLabelsDir(String dataset) {
 		return topicCountCalibrationDir(dataset) + "/lau_doc_labels";
 	}
 	
 	public static String topicCountCalibrationLauDocLabelsFilename(String dataset, int numTopics, int run) {
-		return topicCountCalibrationLauDocLabelsDir(dataset) + "/" + name(numTopics, run) + DOC_LABELS_EXT;
+		return topicCountCalibrationLauDocLabelsDir(dataset) + "/" + name(numTopics, run) + jhn.Paths.DOC_LABELS_EXT;
 	}
 	
 	public static String topicCountCalibrationEdaDocLabelsDir(String dataset) {
@@ -85,15 +81,14 @@ public class Paths {
 	}
 	
 	public static String topicCountCalibrationEdaDocLabelsFilename(String dataset, int numTopics, int run) {
-		return topicCountCalibrationEdaDocLabelsDir(dataset) + "/" + name(numTopics, run) + DOC_LABELS_EXT;
+		return topicCountCalibrationEdaDocLabelsDir(dataset) + "/" + name(numTopics, run) + jhn.Paths.DOC_LABELS_EXT;
 	}
 	
 	public static String topicCountCalibrationDocTopicsDir(String dataset) {
 		return topicCountCalibrationDir(dataset) + "/doctopics";
 	}
 	
-	public static final String DOCTOPICS_EXT = ".doctopics";
 	public static String topicCountCalibrationDocTopicsFilename(String dataset, int numTopics, int run) {
-		return topicCountCalibrationDocTopicsDir(dataset) + "/" + name(numTopics, run) + DOCTOPICS_EXT;
+		return topicCountCalibrationDocTopicsDir(dataset) + "/" + name(numTopics, run) + jhn.Paths.DOCTOPICS_EXT;
 	}
 }
