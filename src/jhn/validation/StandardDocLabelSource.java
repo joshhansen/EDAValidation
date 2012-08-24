d9004ac61a27792d05e8514c1cf7ad35f57afe9b
Josh Hansen <hansen.joshuaa@gmail.com>
2012-08-24 16:16:02 -0600

    Load document text

--- src/jhn/validation/StandardDocLabelSource.java
+++ src/jhn/validation/StandardDocLabelSource.java
@@ -5,8 +5,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import jhn.util.RandUtil;
+import jhn.util.Util;
 
 /**
  * Format:
@@ -16,22 +19,37 @@ import jhn.util.RandUtil;
  */
 public class StandardDocLabelSource implements DocLabelSource, BareLabelSource {
 	private Map<String,String[]> labels = new HashMap<>();
+	private Map<String,String> docText = new HashMap<>();
 	public StandardDocLabelSource(String srcFilename) throws IOException {
 		try(BufferedReader r = new BufferedReader(new FileReader(srcFilename))) {
 			String line;
 			while( (line=r.readLine()) != null) {
 				if(!line.startsWith("#")) {
 					String[] parts = line.split(",");
-					String[] fileParts = parts[1].split("/");
+					String fullFilename = parts[1];
+					String[] fileParts = fullFilename.split("/");
 					String filename = fileParts[fileParts.length-1];
 					String[] labelsArr = new String[parts.length - 2];
 					for(int i = 2; i < parts.length; i++) {
-						labelsArr[i-2] = parts[i];
+						labelsArr[i-2] = stripQuotes(parts[i]);
 					}
 					labels.put(filename, labelsArr);
+					
+					fullFilename = fullFilename.replaceFirst("file:", "");
+					
+					fullFilename = fullFilename.replaceAll("jjfresh", "josh");
+					
+					docText.put(filename, Util.readFile(fullFilename));
 				}
 			}
 		}
+	}
+	
+	private static final Pattern quotedRgx = Pattern.compile("(['\"]?)(.*)\\1");
+	private String stripQuotes(String s) {
+		Matcher m = quotedRgx.matcher(s);
+		m.matches();
+		return m.group(2);
 	}
 
 	@Override
@@ -46,5 +64,9 @@ public class StandardDocLabelSource implements DocLabelSource, BareLabelSource {
 	@Override
 	public String[] labels(int numLabels) {
 		return labels(RandUtil.randItem(labels.keySet()), numLabels);
+	}
+	
+	public String docText(String docFilename) {
+		return docText.get(docFilename);
 	}
 }
