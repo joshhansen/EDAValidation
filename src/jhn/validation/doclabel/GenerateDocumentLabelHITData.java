6ae3af8673e09391cf9d9a0ee01341acd0af781d
Josh Hansen <hansen.joshuaa@gmail.com>
2012-09-15 02:00:46 -0600

    try-with-resources

--- src/jhn/validation/doclabel/GenerateDocumentLabelHITData.java
+++ src/jhn/validation/doclabel/GenerateDocumentLabelHITData.java
@@ -13,19 +13,18 @@ public class GenerateDocumentLabelHITData {
 	private static Int2ObjectMap<String> loadLabels(String topicLabelsFilename) throws Exception {
 		Int2ObjectMap<String> labels = new Int2ObjectOpenHashMap<>();
 		
-		BufferedReader r = new BufferedReader(new FileReader(topicLabelsFilename));
-		
-		String tmp = null;
-		while( (tmp=r.readLine()) != null) {
-			if(!tmp.startsWith("#")) {
-				String[] parts = tmp.split(",");
-				int topicNum = Integer.parseInt(parts[0]);
-				String label = parts[parts.length-1];
-				label = label.replaceAll("[\"]", "");
-				labels.put(topicNum, label);
+		try(BufferedReader r = new BufferedReader(new FileReader(topicLabelsFilename))) {
+			String tmp = null;
+			while( (tmp=r.readLine()) != null) {
+				if(!tmp.startsWith("#")) {
+					String[] parts = tmp.split(",");
+					int topicNum = Integer.parseInt(parts[0]);
+					String label = parts[parts.length-1];
+					label = label.replaceAll("[\"]", "");
+					labels.put(topicNum, label);
+				}
 			}
 		}
-		r.close();
 		
 		return labels;
 	}
@@ -33,30 +32,31 @@ public class GenerateDocumentLabelHITData {
 	private static void generate(String docTopicsFilename, String topicLabelsFilename, String outputFilename, int numTopics) throws Exception {
 		Int2ObjectMap<String> labels = loadLabels(topicLabelsFilename);
 		
-		PrintStream w = new PrintStream(new FileOutputStream(outputFilename));
-		w.print("#docnum,source");
-		for(int i = 0; i < numTopics; i++) {
-			w.print(",label");
-			w.print(i+1);
-		}
-		w.println();
-		
-		BufferedReader r = new BufferedReader(new FileReader(docTopicsFilename));
-		String tmp = null;
-		while( (tmp=r.readLine()) != null) {
-			if(!tmp.startsWith("#")) {
-				String[] parts = tmp.split("\t");
-				
-				w.append(parts[0]).append(',').append(parts[1]);
-				
-				for(int i = 0; i < numTopics; i++) {
-					int topicNum = Integer.parseInt(parts[2*i + 2]);
-					w.append(",\"").append(labels.get(topicNum)).append("\"");
+		try(PrintStream w = new PrintStream(new FileOutputStream(outputFilename))) {
+			w.print("#docnum,source");
+			for(int i = 0; i < numTopics; i++) {
+				w.print(",label");
+				w.print(i+1);
+			}
+			w.println();
+			
+			try(BufferedReader r = new BufferedReader(new FileReader(docTopicsFilename))) {
+				String tmp = null;
+				while( (tmp=r.readLine()) != null) {
+					if(!tmp.startsWith("#")) {
+						String[] parts = tmp.split("\t");
+						
+						w.append(parts[0]).append(',').append(parts[1]);
+						
+						for(int i = 0; i < numTopics; i++) {
+							int topicNum = Integer.parseInt(parts[2*i + 2]);
+							w.append(",\"").append(labels.get(topicNum)).append("\"");
+						}
+						w.println();
+					}
 				}
-				w.println();
 			}
 		}
-		w.close();
 	}
 	
 	public static void main(String[] args) throws Exception {
