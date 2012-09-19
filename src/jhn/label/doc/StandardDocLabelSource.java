101b03c2d358953764dbf1544f21e2bff945952f
Josh Hansen <hansen.joshuaa@gmail.com>
2012-09-18 23:09:35 -0600

    Fix quote handling by treating quotes as part of delimiter

--- src/jhn/label/doc/StandardDocLabelSource.java
+++ src/jhn/label/doc/StandardDocLabelSource.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import jhn.label.BareLabelSource;
@@ -20,28 +19,22 @@ import jhn.util.RandUtil;
 public class StandardDocLabelSource implements DocLabelSource, BareLabelSource {
 	private Map<String,String[]> labels = new HashMap<>();
 	
+	private static final Pattern delimRgx = Pattern.compile("[\",]+");
 	public StandardDocLabelSource(String srcFilename) throws IOException {
 		try(BufferedReader r = new BufferedReader(new FileReader(srcFilename))) {
 			String line;
 			while( (line=r.readLine()) != null) {
 				if(!line.startsWith("#")) {
-					String[] parts = line.split(",");
-					String fullFilename = parts[1].replaceFirst("file:", "");
+					String[] parts = delimRgx.split(line);
+					String fullFilename = parts[1];//.replaceFirst("file:", "");
 					String[] labelsArr = new String[parts.length - 2];
 					for(int i = 2; i < parts.length; i++) {
-						labelsArr[i-2] = stripQuotes(parts[i]);
+						labelsArr[i-2] = parts[i];
 					}
 					labels.put(fullFilename, labelsArr);
 				}
 			}
 		}
-	}
-	
-	private static final Pattern quotedRgx = Pattern.compile("(['\"]?)(.*)\\1");
-	private static String stripQuotes(String s) {
-		Matcher m = quotedRgx.matcher(s);
-		m.matches();
-		return m.group(2);
 	}
 
 	@Override
