1a5708bdf15db91f331623a8fde8273ddcf0eb7f
Josh Hansen <hansen.joshuaa@gmail.com>
2012-09-18 23:08:25 -0600

    Replace custom code with StandardDocLabelSource

--- src/jhn/label/doc/RandomRunsDocLabelSource.java
+++ src/jhn/label/doc/RandomRunsDocLabelSource.java
@@ -1,10 +1,9 @@
 package jhn.label.doc;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileFilter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
@@ -13,47 +12,37 @@ import jhn.util.FileExtensionFilter;
 import jhn.util.RandUtil;
 
 public class RandomRunsDocLabelSource implements DocLabelSource {
-	private Int2ObjectMap<Map<String,String[]>> allLabels = new Int2ObjectOpenHashMap<>();
+	private static final Pattern filenameRgx = Pattern.compile("([^_]+_)?(\\d+)");
 	
+	private Int2ObjectMap<DocLabelSource> allLabels = new Int2ObjectOpenHashMap<>();
 	public RandomRunsDocLabelSource(String docLabelsDir) throws Exception {
-		for(File f : new File(docLabelsDir).listFiles(new FileExtensionFilter(".doclabels"))) {
-			int run = Integer.parseInt(f.getName().split("[.]")[0]);
-			allLabels.put(run, loadDocLabels(f.getPath()));
+		this(docLabelsDir, new FileExtensionFilter(jhn.Paths.DOC_LABELS_EXT));
+	}
+	
+	public RandomRunsDocLabelSource(String docLabelsDir, FileFilter fileFilter) throws Exception {
+		System.out.println(docLabelsDir);
+		for(File f : new File(docLabelsDir).listFiles()) {
+			String baseName = f.getName().split("[.]")[0];
+			Matcher m = filenameRgx.matcher(baseName);
+			m.matches();
+			int run = Integer.parseInt(m.group(2));
+			allLabels.put(run, new StandardDocLabelSource(f.getPath()));
 		}
+	}
+	
+	protected RandomRunsDocLabelSource() {
+		// Override if desired
 	}
 
 	@Override
 	public String[] labels(String docFilename, int numLabels) {
 		int run = RandUtil.randItem(allLabels.keySet().toIntArray());
-		String[] labels = allLabels.get(run).get(docFilename);
+		String[] labels = allLabels.get(run).labels(docFilename, numLabels);
 		
 		String[] selectedLabels = new String[numLabels];
 		for(int i = 0; i < selectedLabels.length; i++) {
 			selectedLabels[i] = labels[i];
 		}
 		return selectedLabels;
-	}
-	
-	private static Map<String,String[]> loadDocLabels(String filename) throws Exception {
-		Map<String,String[]> sources = new HashMap<>();
-		
-		try(BufferedReader r = new BufferedReader(new FileReader(filename))) {
-			String line = null;
-			while( (line=r.readLine()) != null) {
-				if(!line.startsWith("#")) {
-					String[] parts = line.split("[\",]+");
-					
-					// Skip the leading and trailing quotation marks:
-					String[] labels = new String[parts.length - 2];
-					for(int i = 0; i < labels.length; i++) {
-						labels[i] = parts[i+2];
-					}
-					
-					sources.put(parts[0], labels);
-				}
-			}
-		}
-		
-		return sources;
 	}
 }
