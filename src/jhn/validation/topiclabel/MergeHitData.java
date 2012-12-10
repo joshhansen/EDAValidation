63ee5dfbdd7462cc50ff62445c2a3e636725fb57
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:14:45 -0700

    Restrict non-lau merged topic label data to the top 100 topics in the eda output

--- src/jhn/validation/topiclabel/MergeHitData.java
+++ src/jhn/validation/topiclabel/MergeHitData.java
@@ -6,13 +6,20 @@ import java.util.regex.Pattern;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.store.FSDirectory;
 
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
+
+import jhn.eda.EDA1;
+import jhn.idx.IntIndex;
+import jhn.io.TopTopicsReader;
 import jhn.label.LabelSource;
-import jhn.label.topic.LuceneTopicLabelSource;
-import jhn.label.topic.RandomRunsTopicLabelSource;
-import jhn.label.topic.RandomTopicLabelsSource;
+import jhn.label.topic.AggregateTopicLabelSource;
+import jhn.label.topic.RestrictedLuceneTopicLabelSource;
+import jhn.label.topic.RestrictedRandomTopicLabelSource;
 import jhn.label.topic.SampleableTopicLabelSource;
-import jhn.util.RandUtil;
+import jhn.util.Util;
 import jhn.validation.Merger;
+import jhn.validation.Paths;
 
 public class MergeHitData extends Merger<Integer> {
 	private final int chooseFromTopN;
@@ -27,34 +34,35 @@ public class MergeHitData extends Merger<Integer> {
 		System.out.println(modelName(src1));
 		System.out.println(modelName(src2));
 		
-		SampleableTopicLabelSource stls1 = (SampleableTopicLabelSource) src1;
-		SampleableTopicLabelSource stls2 = (SampleableTopicLabelSource) src2;
+		SampleableTopicLabelSource[] sources = new SampleableTopicLabelSource[] {
+				(SampleableTopicLabelSource) src1, (SampleableTopicLabelSource) src2
+		};
 		
-		int topic1 = stls1.randTopicNum();
-		int topic2 = stls2.randTopicNum();
+		int[] topics = new int[2];
+		String[] labels = new String[2];
+		String[][] words = new String[2][];
 		
-		String[] labels1 = stls1.labels(topic1, chooseFromTopN);
-		String[] labels2 = stls2.labels(topic2, chooseFromTopN);
-		
-		String label1 = cleanLabel(RandUtil.randItem(labels1));
-		String label2 = cleanLabel(RandUtil.randItem(labels2));
-		
-		String[][] words = new String[][]{stls1.topicWords(topic1), stls2.topicWords(topic2)};
+		for(int i = 0; i < sources.length; i++) {
+			do {
+				topics[i] = sources[i].randTopicNum();
+				labels[i] = sources[i].labels(topics[i], chooseFromTopN)[0];
+				words[i] = sources[i].topicWords(topics[i]);
+			} while(words[i].length < numTopicWords);
+		}
 		
 		StringBuilder line = new StringBuilder();
 		
-		line.append(modelName(src1)).append(',')
-			.append(topic1).append(',')
-			.append(label1).append(',')
-			.append(modelName(src2)).append(',')
-			.append(topic2).append(',')
-			.append(label2).append(',');
+		for(int i = 0; i < sources.length; i++) {
+			line.append(modelName(sources[i])).append(',')
+				.append(topics[i]).append(',')
+				.append('"').append(labels[i]).append("\",");
+		}
 		
-		for(int side = 0; side < words.length; side++) {
+		for(int side = 0; side < sources.length; side++) {
 			for(int i = 0; i < numTopicWords; i++) {
 				String word = words[side].length > i ? words[side][i] : "";
 				line.append(word);
-				if(i < numTopicWords - 1) {
+				if(side < words.length-1 || i < numTopicWords - 1) {
 					line.append(',');
 				}
 			}
@@ -75,7 +83,7 @@ public class MergeHitData extends Merger<Integer> {
 		
 		for(int i = 0; i < sides.length; i++) {
 			for(int j = 0; j < numTopicWords; j++) {
-				h.append("model").append(sides[i]).append("word").append(j);
+				h.append("model").append(sides[i]).append("word").append(j+1);
 				if(i < sides.length - 1 || j < numTopicWords - 1) {
 					h.append(',');
 				}
@@ -83,46 +91,53 @@ public class MergeHitData extends Merger<Integer> {
 		}
 		
 		return h.toString();
-		
-//		return "model1,model2,"
-//			+ "model1topicnum,model1word1,model1word2,model1word3,model1word4,model1word5,model1word6,model1word7,model1word8,model1word9,model1word10,model1label,"
-//			+ "model2topicnum,model2word1,model2word2,model2word3,model2word4,model2word5,model2word6,model2word7,model2word8,model2word9,model2word10,model2label\n";
 	}
 	
 	public static void main(String[] args) throws Exception {
-//		merge(jhn.Paths.outputDir("EDA") + "/runs/17/hit_data_it95.hit.csv",
-//			  jhn.Paths.outputDir("LauEtAl") + "/reuters-labels.hit.csv",
-//		      jhn.Paths.outputDir("EDAValidation") + "/merged_reuters.hit.csv");
-		
-//		Class<? extends EDA> algo = EDA2.class;
-		final int topicCount = 20;
+		final int topicCount = 10;
 		final String topicWordIdxName = "wp_lucene4";
 //		final String datasetName = "toy_dataset4";
-		final String datasetName = "reuters21578_noblah2";
+//		final String datasetName = "reuters21578_noblah2";
+		String datasetName = "sotu_chunks";
 		String topicWordIdxDir = jhn.Paths.topicWordIndexDir(topicWordIdxName);
 		final int numComparisons = 200;
 		final int chooseFromTopN = 1;
+		final int minCount = 2;
 		
 		final String lauLabelsDir = jhn.validation.Paths.lauTopicLabelsDir(datasetName);
 		
 		
 //		Pattern edaFilenameRgx = Pattern.compile("run(\\d+)_iters\\d+-\\d+\\" + jhn.Paths.DOC_LABELS_EXT);
 //		DocLabelSource eda = new RandomRunsDocLabelSource(edaLabelsDir, edaFilenameRgx);
-		LabelSource<Integer> eda = new LuceneTopicLabelSource(topicWordIdxDir);
+		IntSet allowedTopics = null;
+		try(TopTopicsReader r = new TopTopicsReader(Paths.topTopicsFilename(EDA1.class, datasetName))) {
+			allowedTopics = r.readTopicsSet(100);
+		}
+		
+		String topicMappingFilename = jhn.Paths.topicMappingFilename(topicWordIdxName, datasetName, minCount);
+		IntIndex topicMapping = (IntIndex) Util.deserialize(topicMappingFilename);
+		
+		IntSet actuallyAllowedTopics = new IntOpenHashSet();
+		for(int i : allowedTopics) {
+			actuallyAllowedTopics.add(topicMapping.objectAtI(i));
+		}
+		
+		LabelSource<Integer> eda = new RestrictedLuceneTopicLabelSource(topicWordIdxDir, datasetName, actuallyAllowedTopics);
 		
 		Pattern lauFilenameRgx = Pattern.compile("lda" + topicCount + "topics_(\\d+)\\" + jhn.Paths.TOPIC_LABELS_EXT);
 //		DocLabelSource lauEtAl = new RandomRunsDocLabelSource(lauLabelsDir, lauFilenameRgx);
-		LabelSource<Integer> lauEtAl = new RandomRunsTopicLabelSource(lauLabelsDir, lauFilenameRgx);
+//		LabelSource<Integer> lauEtAl = new RandomRunsTopicLabelSource(lauLabelsDir, lauFilenameRgx);
+		LabelSource<Integer> lauEtAl = new AggregateTopicLabelSource(lauLabelsDir, lauFilenameRgx);
 		
 		try(IndexReader topicWordIdx = IndexReader.open(FSDirectory.open(new File(topicWordIdxDir)))) {
-			LabelSource<Integer> random = new RandomTopicLabelsSource(topicWordIdx);
+			LabelSource<Integer> random = new RestrictedRandomTopicLabelSource(topicWordIdx, datasetName, actuallyAllowedTopics);
 			
 			File outputDir = new File(jhn.validation.Paths.hitDataDir(datasetName));
 			if(!outputDir.exists()) {
 				outputDir.mkdirs();
 			}
 			
-			String outputFilename = jhn.validation.Paths.mergedTopicLabelsFilename(datasetName, numComparisons, chooseFromTopN);
+			String outputFilename = jhn.validation.Paths.mergedTopicLabelsFilename(datasetName, numComparisons, chooseFromTopN) + ".restricted";
 			
 			MergeHitData mhd = new MergeHitData(numComparisons, outputFilename, chooseFromTopN);
 			mhd.addModel(eda, "EDA", 0.45);
