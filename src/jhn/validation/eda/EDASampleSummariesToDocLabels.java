10f004e58cad0beefeae28d12ef7348b6b83ddba
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:13:10 -0700

    Use SampleSummaryFileReader

--- src/jhn/validation/eda/EDASampleSummariesToDocLabels.java
+++ src/jhn/validation/eda/EDASampleSummariesToDocLabels.java
@@ -1,10 +1,6 @@
 package jhn.validation.eda;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -24,8 +20,11 @@ import jhn.counts.i.i.i.IntIntIntCounterMap;
 import jhn.counts.i.i.i.IntIntIntRAMCounterMap;
 import jhn.eda.EDA;
 import jhn.eda.EDA2;
+import jhn.eda.io.SampleSummaryFileReader;
 import jhn.eda.lucene.LuceneLabelAlphabet;
+import jhn.eda.tokentopics.DocTopicCounts;
 import jhn.idx.IntIndex;
+import jhn.io.DocLabelsFileWriter;
 import jhn.util.Util;
 
 public class EDASampleSummariesToDocLabels {
@@ -33,30 +32,36 @@ public class EDASampleSummariesToDocLabels {
 		System.out.println(sampleSummaryFilename);
 		IntIntIntCounterMap counts = new IntIntIntRAMCounterMap();
 		
-		try(BufferedReader r = new BufferedReader(new FileReader(sampleSummaryFilename))) {
-			int docNum;
-			int lineNum = 0;
-			String tmp = null;
-			String[] parts;
-			String[] subparts;
-			while( (tmp=r.readLine()) != null) {
-				if(!tmp.startsWith("#")) {
-					parts = tmp.split("\\s+");
-					
-					docNum = Integer.parseInt(parts[0]);
-					
-					for(int i = 2; i < parts.length; i++) {
-						subparts = parts[i].split(":");
-						counts.set(docNum, Integer.parseInt(subparts[0]), Integer.parseInt(subparts[1]));
-					}
-					
-					lineNum++;
-					System.out.print('.');
-					if(lineNum > 0 && lineNum % 120 == 0) {
-						System.out.println(lineNum);
-					}
+		try(SampleSummaryFileReader r = new SampleSummaryFileReader(sampleSummaryFilename)) {
+			for(DocTopicCounts dtc : r) {
+				IntIntCounter subcounts = counts.getCounter(dtc.docNum());
+				while(dtc.hasNext()) {
+					subcounts.set(dtc.nextInt(), dtc.nextDocTopicCount());
 				}
 			}
+//			int docNum;
+//			int lineNum = 0;
+//			String tmp = null;
+//			String[] parts;
+//			String[] subparts;
+//			while( (tmp=r.readLine()) != null) {
+//				if(!tmp.startsWith("#")) {
+//					parts = tmp.split("\\s+");
+//					
+//					docNum = Integer.parseInt(parts[0]);
+//					
+//					for(int i = 2; i < parts.length; i++) {
+//						subparts = parts[i].split(":");
+//						counts.set(docNum, Integer.parseInt(subparts[0]), Integer.parseInt(subparts[1]));
+//					}
+//					
+//					lineNum++;
+//					System.out.print('.');
+//					if(lineNum > 0 && lineNum % 120 == 0) {
+//						System.out.println(lineNum);
+//					}
+//				}
+//			}
 		}
 		
 		return counts;
@@ -65,18 +70,24 @@ public class EDASampleSummariesToDocLabels {
 	public static Int2ObjectMap<String> docFilenames(String sampleSummaryFilename) throws Exception {
 		Int2ObjectMap<String> sources = new Int2ObjectOpenHashMap<>();
 		
-		try(BufferedReader r = new BufferedReader(new FileReader(sampleSummaryFilename))) {
-			int docNum;
-			String tmp = null;
-			String[] parts;
-			while( (tmp=r.readLine()) != null) {
-				if(!tmp.startsWith("#")) {
-					parts = tmp.split("\\s+");
-					docNum = Integer.parseInt(parts[0]);
-					sources.put(docNum, parts[1]);
-				}
+		try(SampleSummaryFileReader r = new SampleSummaryFileReader(sampleSummaryFilename)) {
+			for(DocTopicCounts dtc : r) {
+				sources.put(dtc.docNum(), dtc.docSource());
 			}
 		}
+		
+//		try(BufferedReader r = new BufferedReader(new FileReader(sampleSummaryFilename))) {
+//			int docNum;
+//			String tmp = null;
+//			String[] parts;
+//			while( (tmp=r.readLine()) != null) {
+//				if(!tmp.startsWith("#")) {
+//					parts = tmp.split("\\s+");
+//					docNum = Integer.parseInt(parts[0]);
+//					sources.put(docNum, parts[1]);
+//				}
+//			}
+//		}
 		
 		return sources;
 	}
@@ -113,9 +124,8 @@ public class EDASampleSummariesToDocLabels {
 								   LabelAlphabet labels, IntIndex topicMapping, int topNlabels, String outputFilename) throws Exception {
 		
 		System.out.println("Writing " + outputFilename);
-		try(PrintStream w = new PrintStream(new FileOutputStream(outputFilename))) {
-			w.println("#docnum,source,topic1label,topic2label,topic3label,topic4label,topic5label,topic6label,topic7label,topic8label,topic9label,topic10label");
-			
+		
+		try(DocLabelsFileWriter w = new DocLabelsFileWriter(outputFilename, topNlabels)) {
 			int topicNum;
 			int globalTopicNum;
 			String label;
@@ -125,29 +135,54 @@ public class EDASampleSummariesToDocLabels {
 			Arrays.sort(entries, cmp);
 			
 			for(Int2ObjectMap.Entry<Counter<Integer,Integer>> entry : entries) {
-				w.print(entry.getIntKey());
-				w.print(',');
-				w.print(docFilenames.get(entry.getIntKey()));
+				w.startDocument(entry.getIntKey(), docFilenames.get(entry.getIntKey()));
 				
 				for(Int2IntMap.Entry count : ((IntIntCounter)entry.getValue()).fastTopN(topNlabels)) {
 					topicNum = count.getIntKey();
 					globalTopicNum = topicMapping.objectAtI(topicNum);
 					label = labels.lookupObject(globalTopicNum).toString();
 					
-					w.print(",\"");
-					w.print(label);
-					w.print("\"");
+					w.label(label);
 				}
-				w.println();
+				w.endDocument();
 			}
-		}//end try
+		}
+		
+//		try(PrintStream w = new PrintStream(new FileOutputStream(outputFilename))) {
+//			w.println("#docnum,source,topic1label,topic2label,topic3label,topic4label,topic5label,topic6label,topic7label,topic8label,topic9label,topic10label");
+//			
+//			int topicNum;
+//			int globalTopicNum;
+//			String label;
+//			
+//			@SuppressWarnings("unchecked")
+//			Int2ObjectMap.Entry<Counter<Integer,Integer>>[] entries = docTopicCounts.int2ObjectEntrySet().toArray(new Int2ObjectMap.Entry[0]);
+//			Arrays.sort(entries, cmp);
+//			
+//			for(Int2ObjectMap.Entry<Counter<Integer,Integer>> entry : entries) {
+//				w.print(entry.getIntKey());
+//				w.print(',');
+//				w.print(docFilenames.get(entry.getIntKey()));
+//				
+//				for(Int2IntMap.Entry count : ((IntIntCounter)entry.getValue()).fastTopN(topNlabels)) {
+//					topicNum = count.getIntKey();
+//					globalTopicNum = topicMapping.objectAtI(topicNum);
+//					label = labels.lookupObject(globalTopicNum).toString();
+//					
+//					w.print(",\"");
+//					w.print(label);
+//					w.print("\"");
+//				}
+//				w.println();
+//			}
+//		}//end try
 	}
 	
 	public static void main(String[] args) throws Exception {
 		Class<? extends EDA> algo = EDA2.class;
 		final int minCount = 2;
 		final String topicWordIdxName = "wp_lucene4";
-		final String datasetName = "sotu_chunks";// toy_dataset2 debates2012 sacred_texts state_of_the_union reuters21578
+		final String datasetName = "reuters21578_noblah2";// toy_dataset2 debates2012 sacred_texts state_of_the_union reuters21578
 		final String summarizer = "sum";
 //		final int run = 17;
 //		final int iteration = 95;
@@ -165,7 +200,7 @@ public class EDASampleSummariesToDocLabels {
 	//		String fastStateFilename =    Paths.fastStateFilename(run, iteration);
 			String sampleSummaryFilename = jhn.eda.Paths.sampleSummaryFilename(summarizer, runDir, firstIter, lastIter, minCount);
 			String topicWordIdxDir =      jhn.Paths.topicWordIndexDir("wp_lucene4");
-			String topicMappingFilename = jhn.eda.Paths.topicMappingFilename(topicWordIdxName, datasetName, minCount);
+			String topicMappingFilename = jhn.Paths.topicMappingFilename(topicWordIdxName, datasetName, minCount);
 			String outputFilename =       jhn.validation.Paths.edaDocLabelsFilename(algo, datasetName, firstIter, lastIter, run);
 			
 			generate(sampleSummaryFilename, topicWordIdxDir, topicMappingFilename, outputFilename, topNlabels);
