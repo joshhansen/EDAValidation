d127736981310ac7e90cf4d3a54362427152dfce
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:11:43 -0700

    Filter lucene topic labels according to the vocabulary of a mallet imported data file. Sort topic words by frequency.

--- src/jhn/label/topic/LuceneTopicLabelSource.java
+++ src/jhn/label/topic/LuceneTopicLabelSource.java
@@ -2,21 +2,38 @@ package jhn.label.topic;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.TermFreqVector;
 import org.apache.lucene.store.FSDirectory;
 
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.InstanceList;
+
 import jhn.util.RandUtil;
+import jhn.util.Util;
 import jhn.wp.Fields;
 
 public class LuceneTopicLabelSource implements SampleableTopicLabelSource {
 	private IndexReader r;
+	private Alphabet typeAlphabet;
 	private final int numTopics;
-	public LuceneTopicLabelSource(String topicWordIdxDir) throws CorruptIndexException, IOException {
-		r = IndexReader.open(FSDirectory.open(new File(topicWordIdxDir)));
+	public LuceneTopicLabelSource(String topicWordIdxDir, String datasetName) throws CorruptIndexException, IOException {
+		this(IndexReader.open(FSDirectory.open(new File(topicWordIdxDir))), datasetName);
+	}
+	
+	public LuceneTopicLabelSource(IndexReader topicWordIdx, String datasetName) {
+		r = topicWordIdx;
 		numTopics = r.numDocs();
+		
+		String datasetFilename = jhn.Paths.malletDatasetFilename(datasetName);
+		InstanceList targetData = InstanceList.load(new File(datasetFilename));
+		typeAlphabet = (Alphabet) targetData.getAlphabet().clone();
+		typeAlphabet.stopGrowth();
 	}
 	
 	@Override
@@ -42,13 +59,41 @@ public class LuceneTopicLabelSource implements SampleableTopicLabelSource {
 	public int randTopicNum() {
 		return RandUtil.rand.nextInt(numTopics);
 	}
+	
+	private static class TermFreq implements Comparable<TermFreq> {
+		String term;
+		int freq;
+		public TermFreq(String term, int freq) {
+			super();
+			this.term = term;
+			this.freq = freq;
+		}
+		@Override
+		public int compareTo(TermFreq o) {
+			return Util.compareInts(o.freq, freq);
+		}
+		
+	}
 
 	@Override
 	public String[] topicWords(int topicNum) {
 		try {
 			TermFreqVector tfv = r.getTermFreqVector(topicNum, Fields.text);
 			if(tfv != null) {
-				return tfv.getTerms();
+				String[] terms = tfv.getTerms();
+				int[] freqs = tfv.getTermFrequencies();
+				List<TermFreq> tfs = new ArrayList<>();
+				for(int i = 0; i < tfv.size(); i++) {
+					if(typeAlphabet.contains(terms[i])) {
+						tfs.add(new TermFreq(terms[i], freqs[i]));
+					}
+				}
+				Collections.sort(tfs);
+				String[] sortedFilteredTerms = new String[tfs.size()];
+				for(int i = 0; i < tfs.size(); i++) {
+					sortedFilteredTerms[i] = tfs.get(i).term;
+				}
+				return sortedFilteredTerms;
 			}
 		} catch(IOException e) {
 			// Pass through
