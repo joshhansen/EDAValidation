a69799470309f5868f141a3761d62a5d380b99ad
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:05:01 -0700

    Drop commented lines

--- src/jhn/label/topic/TopicLabelSource.java
+++ src/jhn/label/topic/TopicLabelSource.java
@@ -3,9 +3,6 @@ package jhn.label.topic;
 import jhn.label.LabelSource;
 
 public interface TopicLabelSource extends LabelSource<Integer> {
-//	@Override
-//	String[] labels(Integer topicNum, int numlabels);
-	
 	String[] labels(int topicNum, int numLabels);
 	
 	public String[] topicWords(int topicNum);
