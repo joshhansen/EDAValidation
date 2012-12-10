aac6650cd04be25213caac0c69b362f0d9874629
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:04:16 -0700

    Make RandomTopicLabelSource extend LuceneTopicLabelSource

--- src/jhn/label/topic/RandomTopicLabelSource.java
+++ src/jhn/label/topic/RandomTopicLabelSource.java
@@ -2,33 +2,32 @@ package jhn.label.topic;
 
 import java.io.IOException;
 
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 
-import jhn.label.LabelSource;
-import jhn.util.RandUtil;
-import jhn.wp.Fields;
+/** Topic label source where labels do not correspond to topic number */
+public class RandomTopicLabelSource extends LuceneTopicLabelSource {
 
-public class RandomTopicLabelsSource implements LabelSource<Integer> {
-	private final int numDocs;
-	private IndexReader topicWordIdx;
-	public RandomTopicLabelsSource(IndexReader topicWordIdx) {
-		this.topicWordIdx = topicWordIdx;
-		numDocs = topicWordIdx.numDocs();
+	public RandomTopicLabelSource(String topicWordIdxDir, String datasetName) throws CorruptIndexException, IOException {
+		super(topicWordIdxDir, datasetName);
 	}
+	
+	public RandomTopicLabelSource(IndexReader topicWordIdx, String datasetName) {
+		super(topicWordIdx, datasetName);
+	}
+
+	@Override
+	public String[] labels(int topicNum, int numLabels) {
+		return super.labels(randTopicNum(), numLabels);
+	}
+
+	@Override
+	public String[] topicWords(int topicNum) {
+		return super.topicWords(randTopicNum());
+	}
+
 	@Override
 	public String[] labels(Integer labelKey, int numLabels) {
-		int docNum = RandUtil.rand.nextInt(numDocs);
-		String label;
-		try {
-			label = topicWordIdx.document(docNum).get(Fields.label);
-		} catch(IOException e) {
-			label = null;
-		}
-		if(label==null) {
-			return new String[0];
-		}
-		return new String[]{label};
-		
+		return super.labels(randTopicNum(), numLabels);
 	}
-
 }
