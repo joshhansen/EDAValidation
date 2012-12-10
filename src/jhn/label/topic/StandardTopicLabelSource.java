cc5c55dc4cf93bc9d4213bcd812eabbdad5f4e97
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:00:35 -0700

    Use TopicLabelFileReader

--- src/jhn/label/topic/StandardTopicLabelSource.java
+++ src/jhn/label/topic/StandardTopicLabelSource.java
@@ -1,15 +1,13 @@
 package jhn.label.topic;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
+import jhn.io.TopicLabel;
+import jhn.io.TopicLabelFileReader;
 import jhn.label.BareLabelSource;
 import jhn.util.RandUtil;
 
@@ -20,22 +18,13 @@ import jhn.util.RandUtil;
  * etc.
  */
 public class StandardTopicLabelSource implements SampleableTopicLabelSource, BareLabelSource {
-	private static final Pattern rgx = Pattern.compile("([^,]+),(.+),\"([^\"]+)\"");
 	private Int2ObjectMap<String> labels = new Int2ObjectOpenHashMap<>();
 	private Int2ObjectMap<String[]> topicWords = new Int2ObjectOpenHashMap<>();
 	public StandardTopicLabelSource(String topicLabelsFilename) throws FileNotFoundException, IOException {
-		try(BufferedReader r = new BufferedReader(new FileReader(topicLabelsFilename))) {
-			Matcher m;
-			int topicNum;
-			String line = null;
-			while( (line=r.readLine()) != null) {
-				if(!line.startsWith("#")) {
-					m = rgx.matcher(line);
-					m.matches();
-					topicNum = Integer.parseInt(m.group(1));
-					topicWords.put(topicNum, m.group(2).split(","));
-					labels.put(topicNum, m.group(3));
-				}
+		try(TopicLabelFileReader r = new TopicLabelFileReader(topicLabelsFilename)) {
+			for(TopicLabel tl : r) {
+				topicWords.put(tl.topicNum(), tl.words());
+				labels.put(tl.topicNum(), tl.label());
 			}
 		}
 	}
