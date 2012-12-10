e0cce47166109e0fdf8466e48c46eef593ceaf78
Josh Hansen <hansen.joshuaa@gmail.com>
2012-12-10 08:04:51 -0700

    Use DocLabelsFileReader

--- src/jhn/label/doc/StandardDocLabelSource.java
+++ src/jhn/label/doc/StandardDocLabelSource.java
@@ -1,12 +1,11 @@
 package jhn.label.doc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
+import jhn.io.DocLabels;
+import jhn.io.DocLabelsFileReader;
 import jhn.label.BareLabelSource;
 import jhn.util.RandUtil;
 
@@ -19,20 +18,11 @@ import jhn.util.RandUtil;
 public class StandardDocLabelSource implements DocLabelSource, BareLabelSource {
 	private Map<String,String[]> labels = new HashMap<>();
 	
-	private static final Pattern delimRgx = Pattern.compile("[\",]+");
 	public StandardDocLabelSource(String srcFilename) throws IOException {
-		try(BufferedReader r = new BufferedReader(new FileReader(srcFilename))) {
-			String line;
-			while( (line=r.readLine()) != null) {
-				if(!line.startsWith("#")) {
-					String[] parts = delimRgx.split(line);
-					String fullFilename = parts[1];//.replaceFirst("file:", "");
-					String[] labelsArr = new String[parts.length - 2];
-					for(int i = 2; i < parts.length; i++) {
-						labelsArr[i-2] = parts[i];
-					}
-					labels.put(fullFilename, labelsArr);
-				}
+		try(DocLabelsFileReader r = new DocLabelsFileReader(srcFilename)) {
+			for(DocLabels dl : r) {
+				String fullFilename = dl.docSource().replaceFirst("file:", "");
+				labels.put(fullFilename, dl.labels());
 			}
 		}
 	}
